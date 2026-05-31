538685bbc88b0e0017eecb07c1513664310be451
helviz <twomoelvisrodney@gmail.com>
2026-05-31 14:46:43 +0300

    deleted Voice Session

--- tts/VoiceSession.py
+++
@@ -1,9 +0,0 @@
-from Audio.AudioOutput import AudioOutput
-
-
-class VoiceSession:
-    def __init__(self, output: AudioOutput):
-        self.output = output
-
-    async def speak(self, audio):
-        await self.output.send_audio(audio)
